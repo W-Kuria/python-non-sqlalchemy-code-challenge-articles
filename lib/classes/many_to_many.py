8c5aba6f134958cbabb045e6a5ae2ad5f755442a
W-Kuria <william.njoroge.kuria@gmail.com>
2025-08-24 23:04:50 +0300

    feat: complete challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,105 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
+        from .many_to_many import Author, Magazine  
+
+        if not isinstance(author, Author):
+            raise Exception("Author must be an Author instance")
+        if not isinstance(magazine, Magazine):
+            raise Exception("Magazine must be a Magazine instance")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise Exception("Title must be a string between 5 and 50 characters")
+
+        self._title = title
         self.author = author
         self.magazine = magazine
-        self.title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
         
 class Author:
+    all = []
+
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name.strip()) == 0:
+            raise Exception("Author name must be a non-empty string")
+        self._name = name
+        Author.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
 
     def articles(self):
-        pass
+        from .many_to_many import Article  
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        from .many_to_many import Article 
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self.articles():
+            return None
+        return list({mag.category for mag in self.magazines()})
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise Exception("Magazine name must be a string")
+        if not (2 <= len(new_name) <= 16):
+            raise Exception("Magazine name must be between 2 and 16 characters")
+        self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_cat):
+        if not isinstance(new_cat, str) or len(new_cat.strip()) == 0:
+            raise Exception("Category must be a non-empty string")
+        self._category = new_cat
 
     def articles(self):
-        pass
+        from .many_to_many import Article
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        if not self.articles():
+            return None
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        authors = [article.author for article in self.articles()]
+        result = [a for a in set(authors) if authors.count(a) > 2]
+        return result if result else None
+
+    @classmethod
+    def top_publisher(cls):
+        from .many_to_many import Article
+        if not Article.all:
+            return None
+        return max(cls.all, key=lambda m: len(m.articles()))
--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python3
-import ipdb;
+from classes.many_to_many import Author, Magazine, Article
+import ipdb
 
-from classes.many_to_many import Article
-from classes.many_to_many import Author
-from classes.many_to_many import Magazine
+a1 = Author("Alice")
+a2 = Author("Bob")
 
-if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+m1 = Magazine("TechLife", "Technology")
+m2 = Magazine("HealthMag", "Wellness")
 
+a1.add_article(m1, "AI in 2025")
+a1.add_article(m2, "Mindful Living")
+a2.add_article(m1, "Quantum Computing")
+a1.add_article(m1, "Future of Robotics")
+a1.add_article(m1, "VR and Society")
 
-    # don't remove this line, it's for debugging!
-    ipdb.set_trace()
+# don't remove this line, it's for debugging!
+ipdb.set_trace()
